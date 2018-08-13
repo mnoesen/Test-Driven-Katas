84394dd76ba62e165906512bd931eaaf9ec960ec
Stasha Yancho <stasha.yancho@gmail.com>
2018-08-13 10:36:46 -0500

    broke the tests

--- wrap.js
+++ wrap.js
@@ -1,7 +1,13 @@
 const wrap = (str, maxLen) => {
     if (str.length <= maxLen) {
         return str;
+    } else {
+        let returnStr = ''
+        for (let i = 0; i < str.length; i+=maxLen) {
+                returnStr += str.slice(i, maxLen)
+            } returnStr += '\n'
+        return returnStr
     }
 }
 
-module.exports = wrap; 
+module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -8,5 +8,9 @@ describe('wrap', ()=> {
 
     it('Return an unchanged sting if the length of the string is less than the column', () => {
         expect(wrap("Short", 100)).to.equal("Short");
+    });
+
+    it('Returns a block of text with no lines longer than column length', () => {
+        expect(wrap("The cat is a small, typically furry, carnivorous mammal.", 10)).to.equal("The cat is\n a small, \ntypically \nfurry, car\nnivorous m\nammal.")
     })
 });
